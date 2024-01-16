799abbb726b4f14d4c37088d0c0e46f6701810c1
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-16 14:24:09 +0600

    Setup initial project

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <div className="">
+      <h1>resume-craft</h1>
+    </div>
+  </React.StrictMode>
+);
