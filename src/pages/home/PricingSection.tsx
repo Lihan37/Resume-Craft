2aeed5725a2975ca4a3abd7e96011b2eb4c8bd18
git config --global user.name shadowfax191 <shahariarhossain0599@gmail.com>
2024-01-18 22:43:48 +0600

    making change in the price section

--- src/pages/home/PricingSection.tsx
+++ src/pages/home/PricingSection.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const PricingSection: React.FC = () => {
   return <div>
-    <div className="py-20 w-[45%] mx-auto space-y-3">
+    <div className="py-14 w-[45%] mx-auto space-y-3">
       <h1 className="text-center text-4xl font-extrabold">PRICING PLAN</h1>
       <p className="text-center text-inherit">Introducing our Resume Builder – an essential tool designed for modern job seekers seeking to make a lasting impression in today's</p>
     </div>
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 justify-between">
-      <div className=" my-20 shadow-md py-8 pl-8 rounded-lg border-t border-l border-l-[#005eb6] border-t-[#005eb6] border-b-2 border-r-2 border-b-[#0084ff] border-r-[#0084ff] space-y-6 absolate">
+      <div className=" my-20 shadow-md  py-8 pl-8 rounded-xl border-t border-l border-l-[#005eb6] border-t-[#005eb6] border-b-2 border-r-2 border-b-[#0084ff] border-r-[#0084ff] space-y-6 absolate">
         <div className="flex justify-between items-center relative -top-16 pl-8">
           <div className="py-4 w-[80%] text-center bg-gradient-to-r from-[#52b7ff] to-[#0084ff] rounded-full uppercase text-white font-semibold text-lg tracking-wider">Basic</div>
         </div>
@@ -21,9 +21,9 @@ const PricingSection: React.FC = () => {
           <button className="py-4 w-full bg-gradient-to-r from-[#52b7ff] to-[#0084ff] rounded-full uppercase text-white font-semibold text-lg tracking-wider">Free</button>
         </div>
       </div>
-      <div className=" my-20 shadow-md py-8 pl-8 rounded-lg border-t border-l border-l-[#005eb6] border-t-[#005eb6] border-b-2 border-r-2 border-b-[#0084ff] border-r-[#0084ff] space-y-6 absolate">
+      <div className=" my-20 shadow-md  py-8 pl-8 rounded-xl border-t border-l border-l-[#005eb6] border-t-[#005eb6] border-b-2 border-r-2 border-b-[#0084ff] border-r-[#0084ff] space-y-6 absolate">
         <div className="flex justify-between items-center relative -top-16 pl-8">
-          <div className="py-4 w-[80%] text-center bg-gradient-to-r from-[#52b7ff] to-[#0084ff] rounded-full uppercase text-white font-semibold text-lg tracking-wider">standart</div>
+          <div className="py-4 w-[80%] text-center bg-gradient-to-r from-[#52b7ff] to-[#0084ff] rounded-full uppercase text-white font-semibold text-lg tracking-wider">standard</div>
         </div>
         <p className="text-sky-900 font-semibold">Enjoy limitless use with interactive export use</p>
         <ul className="space-y-3">
@@ -35,7 +35,7 @@ const PricingSection: React.FC = () => {
           <button className="py-4 w-full bg-gradient-to-r from-[#52b7ff] to-[#0084ff] rounded-full uppercase text-white font-semibold text-lg tracking-wider">$5 BUY</button>
         </div>
       </div>
-      <div className=" my-20 shadow-md py-8 pl-8 rounded-lg border-t border-l border-l-[#005eb6] border-t-[#005eb6] border-b-2 border-r-2 border-b-[#0084ff] border-r-[#0084ff] space-y-6 absolate">
+      <div className=" my-20 shadow-md  py-8 pl-8 rounded-xl border-t border-l border-l-[#005eb6] border-t-[#005eb6] border-b-2 border-r-2 border-b-[#0084ff] border-r-[#0084ff] space-y-6 absolate">
         <div className="flex justify-between items-center relative -top-16 pl-8">
           <div className="py-4 w-[80%] text-center bg-gradient-to-r from-[#52b7ff] to-[#0084ff] rounded-full uppercase text-white font-semibold text-lg tracking-wider">Professional</div>
         </div>
