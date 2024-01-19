bbb054b082499f4db663fe4568b953a101d10432
Eanur Lihan <lihan.lihu@gmail.com>
2024-01-19 11:37:06 +0600

    updated feature

--- src/pages/home/FeatureSection.tsx
+++ src/pages/home/FeatureSection.tsx
@@ -1,7 +1,11 @@
-import React from "react";
 
-const FeatureSection: React.FC = () => {
-  return <div>FeatureSection</div>;
+
+const FeatureSection = () => {
+  return (
+    <div>
+      <h2>Featured section</h2>
+    </div>
+  );
 };
 
 export default FeatureSection;
