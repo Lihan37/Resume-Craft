9c3a43d4c1689699cb5616f73fe14b6300491e7a
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-19 14:05:41 +0600

    refactor: Refactor FaqSection and Testimonials components using constant data
    This commit refactors the FaqSection and Testimonials components to use constant data imported from the 'constant' module. Instead of fetching data from external JSON files, the components now directly use the data provided by the 'constant' module, improving code organization and making the components more modular and reusable.

--- src/pages/home/FaqSection.tsx
+++ src/pages/home/FaqSection.tsx
@@ -1,23 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import SectionHeader from "../../components/common/SectionHeader";
-
-interface FaqItem {
-  id: number;
-  question: string;
-  answer: string;
-}
+import { data } from "../../constant";
 
 const FaqSection: React.FC = () => {
-  const [faqData, setFaqData] = useState<FaqItem[]>([]);
   const [openIndex, setOpenIndex] = useState<number | null>(null);
-
-  useEffect(() => {
-    fetch("/src/data/faq.json")
-      .then((response) => response.json())
-      .then((data: FaqItem[]) => setFaqData(data))
-      .catch((error) => console.error("Error fetching FAQ data:", error));
-  }, []);
 
   const toggleAccordion = (index: number) => {
     setOpenIndex(openIndex === index ? null : index);
@@ -27,7 +14,7 @@ const FaqSection: React.FC = () => {
     <div className="max-w-screen-lg mx-auto py-12">
       <SectionHeader> Frequently Asked Questions</SectionHeader>
       <div className="space-y-4">
-        {faqData.map((item, index) => (
+        {data.faqs.map((item, index) => (
           <div
             key={item.id}
             className={`border p-6 rounded shadow-md shadow-c-primary-light transition-transform transform hover:scale-101`}>
--- src/pages/home/Testimonials.tsx
+++ src/pages/home/Testimonials.tsx
@@ -1,29 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 import SectionHeader from "../../components/common/SectionHeader";
-
-interface TestimonialsItem {
-  id: number;
-  name: string;
-  review: string;
-  image: string;
-  star: number;
-  date: string;
-}
+import { data } from "../../constant";
 
 const Testimonials: React.FC = () => {
-  const [testimonials, setTestimonials] = useState<TestimonialsItem[]>([]);
-
-  useEffect(() => {
-    fetch("/src/data/testimonials.json")
-      .then((response) => response.json())
-      .then((data: TestimonialsItem[]) => setTestimonials(data))
-      .catch((error) =>
-        console.error("Error fetching testimonials data:", error)
-      );
-  }, []);
-
   return (
     <div className="my-20">
       <h1 className="text-2xl text-center text-c-primary font-bold">
@@ -32,7 +13,7 @@ const Testimonials: React.FC = () => {
       <SectionHeader>Reviewed By The Community</SectionHeader>
 
       <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16 ">
-        {testimonials.map((testimonial) => (
+        {data.testimonials.map((testimonial) => (
           <div
             key={testimonial.id}
             // testimonial={testimonial}
