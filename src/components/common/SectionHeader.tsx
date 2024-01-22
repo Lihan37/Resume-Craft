3d3d77c2f9fec14f1899101e0e06fb5839a9d94a
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-22 13:09:50 +0600

    refactor: Update SectionHeader component
    - Modified SectionHeader component to accept an optional 'label' prop
    - If 'label' prop is provided, display an uppercase label above the main header
    - Adjusted styling for improved spacing and responsiveness

--- src/components/common/SectionHeader.tsx
+++ src/components/common/SectionHeader.tsx
@@ -2,10 +2,20 @@ import React from "react";
 
 interface ISectionHeader {
   children: React.ReactNode;
+  label?: string;
 }
-const SectionHeader: React.FC<ISectionHeader> = ({ children }) => {
+const SectionHeader: React.FC<ISectionHeader> = ({ children, label }) => {
   return (
-    <h2 className="text-5xl text-center font-semibold mt-3">{children}</h2>
+    <>
+      {label && (
+        <h1 className="uppercase text-xl text-center font-mono text-c-primary font-bold">
+          {label}
+        </h1>
+      )}
+      <h2 className="mt-5 text-center md:mt-8 lg:mt-10 text-c-dark font-semibold text-xl md:text-5xl lg:text-6xl xl:text-7xl">
+        {children}
+      </h2>
+    </>
   );
 };
 
