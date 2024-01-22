aa323a13fdbdfe8ca28bbdc914bbfb6cf1a552d7
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-22 13:11:39 +0600

    feat: Implement Testimonials component
    - Added Testimonials component with a SectionHeader, TextGradient, ShowStarRating, and Slider components.
    - The component displays overall ratings, reviews, and Trustpilot ratings.
    - The layout is responsive, adjusting for different screen sizes.
    - Commented out a section that seems to be related to rendering individual testimonials.

---
+++ src/pages/home/testimonials/Slider.tsx
@@ -0,0 +1,94 @@
+import React, { useRef } from "react";
+import { data } from "../../../constant";
+import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
+import { Scrollbar, Navigation } from "swiper/modules";
+import { IoArrowBack, IoArrowForward } from "react-icons/io5";
+
+import "swiper/swiper-bundle.css";
+import "./style.css";
+import ShowStarRating, {
+  StarSize,
+} from "../../../components/common/ShowStarRating";
+
+const Button: React.FC<{
+  left?: string;
+  right?: string;
+  onClick: () => void;
+  children: React.ReactNode;
+}> = ({ onClick, left, right, children }) => (
+  <button
+    className="p-2 absolute  transform bottom-10 lg:bottom-11 xl:bottom-8 2xl:bottom-6 text-white flex justify-center items-center z-50 text-xl xl:text-4xl bg-c-primary-light hover:bg-c-primary duration-300 transition-colors rounded-full"
+    style={{ left: left, right: right }}
+    onClick={onClick}>
+    {children}
+  </button>
+);
+const Slider: React.FC = () => {
+  const sliderRef = useRef<SwiperRef>();
+
+  const handleSlideNext = () => {
+    sliderRef.current?.swiper.slideNext();
+  };
+
+  const handleSlidePrev = () => {
+    sliderRef.current?.swiper.slidePrev();
+  };
+  return (
+    <Swiper
+      ref={sliderRef as React.RefObject<SwiperRef>}
+      spaceBetween={30}
+      scrollbar={{ draggable: true }}
+      navigation={{
+        prevEl: ".swiper-prev",
+        nextEl: ".swiper-next",
+      }}
+      loop={true}
+      modules={[Scrollbar, Navigation]}
+      breakpoints={{
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }}
+      className="mySwiper">
+      {data.reviews.map((review) => (
+        <SwiperSlide key={review.id}>
+          <div className=" w-full xl:px-5 flex flex-col gap-5 items-start justify-between border md:border-0 p-2 py-3 md:p-0">
+            <div className=" w-full flex flex-col gap-2 items-start justify-between">
+              <ShowStarRating
+                size={StarSize.MD}
+                rating={review.star}
+                activeColor={review.color}
+              />
+              <h1 className=" font-semibold text-xl">
+                {review.title.slice(0, 22)}
+                {review.title.length > 22 && " ..."}
+              </h1>
+              <h2 className=" text-base font-mono fonts-">
+                {review.review.slice(0, 150)}
+                {review.review.length > 150 && " ..."}
+              </h2>
+            </div>
+            <h1 className=" text-sm text-c-dark-light">
+              {review.name} . {review.date}
+            </h1>
+          </div>
+        </SwiperSlide>
+      ))}
+
+      <Button onClick={handleSlidePrev} left="0%">
+        <IoArrowBack />
+      </Button>
+      <Button onClick={handleSlideNext} left="14%">
+        <IoArrowForward />
+      </Button>
+    </Swiper>
+  );
+};
+
+export default Slider;
---
+++ src/pages/home/testimonials/Testimonials.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import SectionHeader from "../../../components/common/SectionHeader";
+import TextGradient from "../../../components/common/TextGradient";
+import Slider from "./Slider";
+import ShowStarRating from "../../../components/common/ShowStarRating";
+import { AiFillStar } from "react-icons/ai";
+
+const Testimonials: React.FC = () => {
+  return (
+    <div className="my-20 text-c-dark">
+      <SectionHeader label="Testimonials">
+        Reviewed by the <TextGradient>community</TextGradient>{" "}
+        <br className=" hidden md:block" /> Trusted by professionals
+      </SectionHeader>
+
+      <div className=" my-16">
+        <div className="flex flex-col gap-5 md:flex-row justify-between items-start ">
+          <div className="w-full md:w-3/12 text-center flex justify-start items-center flex-col gap-2">
+            <h1 className=" font-semibold text-2xl">4.5 out of 5</h1>
+            <ShowStarRating
+              rating={4}
+              activeColor="#00b67a"
+              deActiveColor="#b8becc"
+            />
+            <div className="flex justify-start items-center text-2xl font-bold mt-5">
+              <AiFillStar className=" text-5xl text-[#00b67a]" />
+              <span>Trustpilot</span>
+            </div>
+            <h3 className=" text-sm text-c-dark-light">
+              based on 45,541 reviews
+            </h3>
+          </div>
+          <div className="w-full md:w-9/12">
+            <Slider />
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16 ">
+        {data.testimonials.map((testimonial) => (
+          <div
+            key={testimonial.id}
+            // testimonial={testimonial}
+            className="bg-[#C8D7EF] p-8 rounded-xl">
+            <Rating
+              style={{ maxWidth: 120 }}
+              value={testimonial.star}
+              readOnly
+            />
+            <h2 className="text-justify my-8">{testimonial.review}</h2>
+            <div className="flex gap-5 items-center ">
+              <img
+                className="w-[60px] rounded-full"
+                src={testimonial.image}
+                alt=""
+              />
+              <div>
+                <h3 className="font-semibold">{testimonial.name}</h3>
+                <p className="text-sm">{testimonial.date}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div> */}
+    </div>
+  );
+};
+
+export default Testimonials;
