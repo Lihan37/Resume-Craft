31d7238752acc11627db173ef9e56d87e16f81a0
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-24 21:57:25 +0600

    Enhance Breadcrumbs component with styling and navigation link
    This commit improves the Breadcrumbs component with the following changes:
    - Adds styling for better visual representation.
    - Utilizes the  icon for a clear indication of navigation.
    - Converts the entire component into a clickable  element for better accessibility and user experience.
    - Updates the font size for better readability on larger screens.

---
+++ src/components/common/Breadcrumbs.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+interface IBreadcrumbs {
+  back: string;
+  label: string;
+}
+
+const Breadcrumbs: React.FC<IBreadcrumbs> = ({ back, label }) => {
+  return (
+    <Link
+      to={back}
+      className="flex justify-start items-center gap-1 lg:gap-3 font-semibold text-base lg:text-xl text-c-dark-light">
+      <span>{label}</span>
+      <MdOutlineArrowForwardIos className="text-base lg:text-xl mt-1" />
+    </Link>
+  );
+};
+
+export default Breadcrumbs;
