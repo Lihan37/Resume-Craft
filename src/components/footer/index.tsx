ab7593112a458faa1c2748f77dc3f32de9c3f746
bikash-dev <bikash.dev.bk@gmail.com>
2024-01-25 00:10:07 +0600

    footer responsive issue solved

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Logo from "../common/Logo";
+import { IoArrowForward } from "react-icons/io5";
 import {
   FaTwitter,
   FaLinkedinIn,
   FaFacebookF,
   FaInstagram,
 } from "react-icons/fa";
-import Button from "../common/Button";
 import { Container } from "../common/Container";
 
 const Footer: React.FC = () => {
@@ -20,43 +20,49 @@ const Footer: React.FC = () => {
           <div className="flex items-center space-x-4">
             <a
               href="#"
-              className="bg-c-primary rounded-full p-3 hover:bg-c-primary-light transition duration-300">
+              className="bg-c-primary rounded-full p-3 hover:bg-c-primary-light transition duration-300"
+            >
               <FaFacebookF className="text-xl text-white" />
             </a>
             <a
               href="#"
-              className="bg-c-primary rounded-full p-3 hover:bg-c-primary-light transition duration-300">
+              className="bg-c-primary rounded-full p-3 hover:bg-c-primary-light transition duration-300"
+            >
               <FaInstagram className="text-xl text-white" />
             </a>
             <a
               href="#"
-              className="bg-c-primary rounded-full p-3 hover:bg-c-primary-light transition duration-300">
+              className="bg-c-primary rounded-full p-3 hover:bg-c-primary-light transition duration-300"
+            >
               <FaLinkedinIn className="text-xl text-white" />
             </a>
             <a
               href="#"
-              className="bg-c-primary rounded-full p-3 hover:bg-c-primary-light transition duration-300">
+              className="bg-c-primary rounded-full p-3 hover:bg-c-primary-light transition duration-300"
+            >
               <FaTwitter className="text-xl text-white" />
             </a>
           </div>
         </div>
         <hr className="h-[3px] bg-gray-400 my-12" />
-        <div className="flex gap-20 flex-col md:flex-row justify-between">
+        <div className="flex gap-5 lg:gap-16 flex-col md:flex-row justify-between">
           <div>
             <h2 className="text-2xl font-bold">Stay Updates</h2>
             <p className="text-gray-600 my-3 ">
               Join our newsletter to stay up to date on features and <br />{" "}
               releases
             </p>
-            <div className="my-4">
+            <div className="flex lg:gap-5 justify-between border border-gray-600 bg-white py-[2px] lg:py-2 my-4 rounded-full">
               <input
                 type="text"
                 name=""
                 id=""
-                className="border border-gray-600 p-2 mr-0 md:mr-2 mb-2 lg:mb-0 rounded-lg"
-                placeholder="Enter your email"
+                className=" px-4 py-2 mr-0 md:mr-2 mb-2 lg:mb-0 outline-none bg-transparent"
+                placeholder="ENTER YOUR EMAIL"
               />
-              <Button>Subscribe</Button>
+              <button className="text-white bg-c-primary px-4 lg:p-3 rounded-full mr-4">
+                <IoArrowForward />
+              </button>
             </div>
             <p>
               <small className="text-gray-600">
@@ -65,7 +71,7 @@ const Footer: React.FC = () => {
               </small>
             </p>
           </div>
-          <div className="grid gap-20 grid-cols-2 md:grid-cols-4">
+          <div className="grid gap-5 lg:gap-16 grid-cols-2 md:grid-cols-4">
             <div>
               <h3 className="font-bold">Resume</h3>
               <div className="flex flex-col text-gray-600 mt-6 space-y-3">
@@ -83,7 +89,7 @@ const Footer: React.FC = () => {
               </div>
             </div>
             <div>
-              <h3 className="font-bold">Our Company</h3>
+              <h3 className="font-bold">Company</h3>
               <div className="flex flex-col text-gray-600 mt-6 space-y-3">
                 <a href="">About Us</a>
                 <a href="">Pricing</a>
