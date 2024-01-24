97f964d3b74a046f25339f24f9278894b243600d
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-24 21:57:53 +0600

    Update Logo component with conditional rendering
    This commit enhances the Logo component by introducing the following changes:
    - Adds an optional prop 'name' to control whether to display the 'ResumeCraft' text.
    - Makes the 'ResumeCraft' text conditional based on the 'name' prop for better flexibility.
    - Improves the component to conditionally render the logo and text based on the 'name' prop.

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { images } from "../../constant";
 import { Link } from "react-router-dom";
 
-const Logo: React.FC = () => {
+interface ILogo {
+  name?: boolean;
+}
+
+const Logo: React.FC<ILogo> = ({ name = true }) => {
   return (
     <Link to="/" className=" flex justify-start items-center gap-2">
       <img className="w-12 h-12 xl:w-14 xl:h-14" src={images.logo} alt="logo" />
-      <h1 className=" font-bold text-3xl xl:text-4xl  text-c-dark">
-        Resume<span className=" text-c-primary">Craft</span>
-      </h1>
+      {name && (
+        <h1 className=" font-bold text-3xl xl:text-4xl  text-c-dark">
+          Resume<span className=" text-c-primary">Craft</span>
+        </h1>
+      )}
     </Link>
   );
 };
