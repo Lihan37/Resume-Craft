78c2449fc4a046bdb519259589a97e97062bd2b4
bikash-dev <bikash.dev.bk@gmail.com>
2024-01-25 21:12:07 +0600

    blog type difine

--- src/components/common/BlogCard.tsx
+++ src/components/common/BlogCard.tsx
@@ -1,12 +1,31 @@
-const BlogCard = ({ blog }) => {
+import React from "react";
+
+interface BlogCardProps {
+  blog: {
+    id: number | string;
+    image: string;
+    title: string;
+    author: string;
+    authorImage: string;
+    company: string;
+    content: string;
+  };
+}
+
+const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   const { image, title, author, authorImage, company, content } = blog;
+
   return (
     <div className="">
       <img src={image} className="rounded-tl-2xl rounded-tr-2xl" alt="" />
       <div className="border-2 border-gray-300 rounded-bl-2xl rounded-br-2xl p-4 md:p-3 lg:p-5">
         <div className="flex gap-4 font-semibold mb-3">
-          <p className="bg-[#f8e9e9] px-4 md:px-3 lg:px-4 py-2 md:py-[4px] lg:py-2 rounded-xl">News</p>
-          <p className="bg-[#c8e6f5] px-4 md:px-3 lg:px-4 py-2 md:py-[4px] lg:py-2 rounded-xl">Inspiration</p>
+          <p className="bg-[#f8e9e9] px-4 md:px-3 lg:px-4 py-2 md:py-[4px] lg:py-2 rounded-xl">
+            News
+          </p>
+          <p className="bg-[#c8e6f5] px-4 md:px-3 lg:px-4 py-2 md:py-[4px] lg:py-2 rounded-xl">
+            Inspiration
+          </p>
         </div>
         <h2 className="text-base lg:text-2xl font-bold">{title}</h2>
         <p className="text-base lg:text-lg text-justify my-5">
