4e99e9a8b0f4333cf2ec0a16e8da2721cf8fa1bb
git config --global user.name shadowfax191 <shahariarhossain0599@gmail.com>
2024-01-25 16:51:48 +0600

    cover letter header pic changing

--- src/pages/templates/coverletter/CoverLetterTemplates.tsx
+++ src/pages/templates/coverletter/CoverLetterTemplates.tsx
@@ -9,7 +9,7 @@ const CoverLetterTemplates: React.FC = () => {
   return (
     <div>
       <div className=" bg-blue-50 "> 
-          <HeaderResume sectionHeader="Craft impactful cover letters for success in your career exploration" description=" Craft impactful cover letters effortlessly with our intuitive generator tool. Tailor each letter to your industry, and level up your job application game. Whether you're a seasoned professional or a recent graduate, our collection of templates and writing guides ensures you make a lasting impression on employers." button="Create Cover letter" image="https://i.ibb.co/CKYSC6n/Untitled-design-7.png"/>
+          <HeaderResume sectionHeader="Craft impactful cover letters for success in your career exploration" description=" Craft impactful cover letters effortlessly with our intuitive generator tool. Tailor each letter to your industry, and level up your job application game. Whether you're a seasoned professional or a recent graduate, our collection of templates and writing guides ensures you make a lasting impression on employers." button="Create Cover letter" image="https://i.ibb.co/71nyXjj/Untitled-design-8.png"/>
         </div>
         <Search/>
         <Popular></Popular>
