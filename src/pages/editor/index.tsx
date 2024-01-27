e1dd920aed9aed9cf01b4efb97171f844f7cba8e
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-28 03:34:13 +0600

    Implement ResumeEditor component
    This commit introduces the ResumeEditor component, which includes sections for 'Personal Information', 'Professional Summary', and 'Employment History'. The component utilizes the Accordion component to organize and display each section with collapsible panels.
    
    For 'Personal Information', various input fields such as job title, name, email, phone number, country, city, address, postal code, driving license, nationality, place of birth, and date of birth are provided. Additional details are hidden by default and can be expanded by clicking the 'Edit additional details' button.
    
    The 'Professional Summary' section features an InputTextEditor for entering professional summary content.
    
    The 'Employment History' section includes input fields for job title, employer, date range, city, and an InputTextEditor for job details.
    
    The commit ensures a responsive and clean design, providing users with an organized and user-friendly resume editing interface.

---
+++ src/pages/editor/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LeftSideBar from "./leftBar/LeftSideBar";
+import useTitleSet from "../../hooks/useTitleSet";
+import ResumeTemplates from "./resume/ResumeTemplates";
+import ResumeEditor from "./resume/ResumeEditor";
+
+const Editor: React.FC = () => {
+  useTitleSet("Resume Builder");
+  return (
+    <>
+      <LeftSideBar editor={<ResumeEditor />} templates={<ResumeTemplates />} />
+    </>
+  );
+};
+
+export default Editor;
---
+++ src/pages/editor/resume/ResumeEditor.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import InputText from "../../../components/common/InputText";
+import Accordion from "../../../components/accordion";
+import AccordionItem from "../../../components/accordion/AccordionItem";
+import AccordionHeader from "../../../components/accordion/AccordionHeader";
+import AccordionPanel from "../../../components/accordion/AccordionPanelProps";
+import { AnimatePresence, motion } from "framer-motion";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import InputTextEditor from "../../../components/common/InputTextEditor";
+import InputMonthYear from "../../../components/common/InputMonthYear";
+
+// const initialState = {
+//   personal: {
+//     jobTitle: "",
+//     picture: "",
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     phone: "",
+//     country: "",
+//     city: "",
+//     address: "",
+//     postalCode: "",
+//     drivingLicense: "",
+//     nationality: "",
+//     placeOfBirth: "",
+//     dateOfBirth: "",
+//   },
+// };
+
+const ResumeEditor: React.FC = () => {
+  // const [state, setState] = useState(initialState);
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div>
+      <div className="">
+        <Accordion defaultIndex={0}>
+          {/* Personal Information  */}
+          <AccordionItem>
+            <AccordionHeader>Personal Information</AccordionHeader>
+            <AccordionPanel>
+              <div className=" space-y-3 py-5">
+                <InputText placeholder="Job Title" />
+                <InputText placeholder="First Name" />
+                <InputText placeholder="Last Name" />
+                <InputText placeholder="Email Address" />
+                <InputText placeholder="Phone Number" />
+                <InputText placeholder="Your Country" />
+
+                <AnimatePresence initial={false}>
+                  {isActive && (
+                    <motion.div
+                      initial={{ height: 0 }}
+                      animate={{ height: "auto" }}
+                      exit={{ height: 0 }}
+                      transition={{ type: "spring", duration: 0.4, bounce: 0 }}>
+                      <div className="space-y-3">
+                        <InputText placeholder="Your City" />
+                        <InputText placeholder="Your Address" />
+                        <InputText placeholder="Postal Code" />
+                        <InputText placeholder="Driving License" />
+                        <InputText placeholder="Nationality" />
+                        <InputText placeholder="Place Of Birth" />
+                        <InputText placeholder="Date Of Birth" />
+                      </div>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+
+                <button
+                  onClick={() => setIsActive((prev) => !prev)}
+                  className=" hover:text-blue-800 duration-300 transition-colors font-semibold text-base text-c-primary cursor-pointer flex justify-start items-center gap-3">
+                  Edit additional details{" "}
+                  {!isActive ? (
+                    <IoIosArrowDown className=" text-xl" />
+                  ) : (
+                    <IoIosArrowUp className=" text-xl" />
+                  )}
+                </button>
+              </div>
+            </AccordionPanel>
+          </AccordionItem>
+          {/* Professional Summary */}
+          <AccordionItem>
+            <AccordionHeader>Professional Summary</AccordionHeader>
+            <AccordionPanel>
+              <div className="py-5">
+                <InputTextEditor />
+              </div>
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem>
+            <AccordionHeader>Employment History</AccordionHeader>
+            <AccordionPanel>
+              <div className="py-5 space-y-5">
+                <InputText placeholder="Job Title" />
+                <InputText placeholder="Employer" />
+                <div className="flex justify-between items-center gap-1">
+                  <InputMonthYear />
+                  <InputMonthYear dropdownLef="-50%" />
+                </div>
+                <InputText placeholder="City" />
+                <InputTextEditor height="160px" />
+              </div>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
+
+export default ResumeEditor;
