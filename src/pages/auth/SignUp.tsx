467b879a6ff2f551e1548f49cda3601723d6c3b4
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-01-27 16:48:17 +0600

    Refactor SignUp component
    This commit refactors the SignUp component by extracting the sign-up form into a separate form component. The extracted form component is now imported as the main body of the SignUp component. This improves code organization and reusability.
    
    Changes:
    - Extracted sign-up form into a separate component.
    - Updated the SignUp component to use the new form component.
    - Replaced the submit button with a common Button component for consistency.

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
-import { FaGoogle, FaLinkedin, FaGithub } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
+import Button from "../../components/common/Button";
 
 const SignUp: React.FC = () => {
   const authContext = useContext(AuthContext);
@@ -20,8 +19,6 @@ const SignUp: React.FC = () => {
 
     try {
       await authContext?.createUser(email, password);
-
-      // Clear input fields after sign up
       setName("");
       setEmail("");
       setPassword("");
@@ -31,110 +28,48 @@ const SignUp: React.FC = () => {
     }
   };
 
-  const handleSocialSignIn = async (provider: string) => {
-    try {
-      if (provider === "google") {
-        await authContext?.googleSignIn();
-      } else {
-        // Handle other social sign-in methods
-      }
-    } catch (error) {
-      console.error(`Error signing in with ${provider}:`, error);
-    }
-  };
-
   return (
-    <div className="flex items-center justify-center my-20">
-      <div className="flex min-h-full mx-auto max-w-screen-md bg-white rounded-2xl shadow-xl w-full">
-        <div className="w-2/3 p-8 text-center">
-          <Link to="/">
-            <h2 className="font-bold text-left ml-2 text-c-dark mb-6 text-xl">
-              Resume<span className="text-c-primary">Craft</span>
-            </h2>
-          </Link>
-          <h2 className="text-2xl text-c-primary font-bold mb-4">
-            Sign Up for an account
-          </h2>
-
-          {/* Social Media Icons */}
-          <div className="flex justify-center mb-4">
-            <button
-              className="bg-red-500 text-white p-4 rounded-full mx-2"
-              onClick={() => handleSocialSignIn("google")}>
-              <FaGoogle size={18} />
-            </button>
-            <button
-              className="bg-blue-500 text-white p-4 rounded-full mx-2"
-              onClick={() => console.log("LinkedIn clicked")}>
-              <FaLinkedin size={18} />
-            </button>
-            <button
-              className="bg-black text-white p-4 rounded-full mx-2"
-              onClick={() => console.log("GitHub clicked")}>
-              <FaGithub size={18} />
-            </button>
-          </div>
-
-          <p className="my-4 text-gray-700">
-            Or, sign up using your email account.
-          </p>
-
-          {/* Sign Up Form */}
-          <form onSubmit={handleSignUp} className="mx-auto">
-            <div className="mb-4">
-              <input
-                className="w-3/4 mx-auto px-3 py-2 border rounded-md"
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                className="w-3/4 mx-auto px-3 py-2 border rounded-md"
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                className="w-3/4 mx-auto px-3 py-2 border rounded-md"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                className="w-3/4 mx-auto px-3 py-2 border rounded-md"
-                type="password"
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </div>
-            <button
-              className="bg-blue-500 w-2/4 text-white px-4 py-2 rounded-3xl"
-              type="submit">
-              Sign Up
-            </button>
-          </form>
-        </div>
-
-        <div className="w-1/3 p-8 rounded-r-2xl bg-c-primary flex flex-col items-center justify-center text-center text-white">
-          <p>Already have an account?</p>
-          <Link to="/login">
-            <button className="text-white border border-white rounded-3xl px-4 py-2 mt-4 mb-6">
-              Log In
-            </button>
-          </Link>
-        </div>
+    <form onSubmit={handleSignUp} className="mx-auto">
+      <div className="mb-4">
+        <input
+          className="w-3/4 mx-auto px-3 py-2 border rounded-md"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
-    </div>
+      <div className="mb-4">
+        <input
+          className="w-3/4 mx-auto px-3 py-2 border rounded-md"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <input
+          className="w-3/4 mx-auto px-3 py-2 border rounded-md"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <input
+          className="w-3/4 mx-auto px-3 py-2 border rounded-md"
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </div>
+      <div className=" flex justify-center items-center w-full">
+        <Button icon={false}>Sign Up</Button>
+      </div>
+    </form>
   );
 };
 
