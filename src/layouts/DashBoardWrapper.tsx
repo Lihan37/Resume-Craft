a4fbb3657466e9cf4440e83a066c5f51e16733c9
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-02 15:27:32 +0600

    Remove unused file

--- src/layouts/DashBoardWrapper.tsx
+++ src/layouts/DashBoardWrapper.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/admin/Sidebar";
-import Navbar from "../components/admin/Navbar";
 
 export const DashBoardWrapper: React.FC = () => {
   return (
     <>
-      {/* <Navbar />
-      <Sidebar /> */}
       <Outlet />
     </>
   );
