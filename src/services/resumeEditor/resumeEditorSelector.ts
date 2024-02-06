5dc344eb956b92717881073a1ca46f87b4f85f95
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-07 02:50:49 +0600

    Create Redux slice for managing resume editor state
    This commit adds the resumeEditorSlice file, which defines a Redux slice using the createSlice function from @reduxjs/toolkit. The slice manages the state of the resume editor in a Redux store.
    
    The initial state includes isLoading, isSyncing, error, and resume fields. isLoading and isSyncing are boolean flags to indicate loading and syncing states, error stores any potential errors, and resume holds the resume data.
    
    The slice defines several reducer functions to update specific parts of the resume data, such as setResume, setError, setWorkExperience, setEducation, setSkills, setLanguage, setReference, and setSocialProfile.
    
    These actions can be dispatched to update the corresponding parts of the resume data in the Redux store.
    
    This slice provides a centralized state management solution for the resume editor, making it easier to manage and manipulate resume data across different components.

---
+++ src/services/resumeEditor/resumeEditorSelector.ts
@@ -0,0 +1,19 @@
+import { RootState } from "../../app/store";
+
+export const selectWorkExperience = (state: RootState) =>
+  state.resumeEditor.resume.workExperience;
+
+export const selectEducation = (state: RootState) =>
+  state.resumeEditor.resume.educations;
+
+export const selectReference = (state: RootState) =>
+  state.resumeEditor.resume.references;
+
+export const selectLanguage = (state: RootState) =>
+  state.resumeEditor.resume.languages;
+
+export const selectSocialProfile = (state: RootState) =>
+  state.resumeEditor.resume.socialProfiles;
+
+export const selectSkills = (state: RootState) =>
+  state.resumeEditor.resume.skills;
---
+++ src/services/resumeEditor/resumeEditorSlice.ts
@@ -0,0 +1,123 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  IResumePersonalInfo,
+  TypeOfLanguage,
+  TypeOfReference,
+  TypeOfSingleEducationHistory,
+  TypeOfSingleEmploymentHistory,
+  TypeOfSingleSocialWebSite,
+  TypeOfSkill,
+} from "../../types/resumeEditor";
+
+interface IResumeData {
+  id: number;
+  type: string;
+  template: string | null;
+  avatar: string | null;
+  personalInfo: IResumePersonalInfo;
+  professionalSummary: string | null;
+  workExperience: TypeOfSingleEmploymentHistory[];
+  skills: TypeOfSkill[];
+  languages: TypeOfLanguage[];
+  references: TypeOfReference[];
+  educations: TypeOfSingleEducationHistory[];
+  socialProfiles: TypeOfSingleSocialWebSite[];
+}
+
+interface IResumeEditorState {
+  isLoading: boolean;
+  isSyncing: boolean;
+  error: string | null;
+  resume: IResumeData;
+}
+
+const initialState: IResumeEditorState = {
+  isLoading: false,
+  isSyncing: false,
+  error: null,
+  resume: {
+    id: 0,
+    type: "",
+    template: null,
+    avatar: null,
+    personalInfo: {
+      jobTitle: "",
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      country: "",
+      city: "",
+      address: "",
+      postalCode: "",
+      drivingLicense: "",
+      nationality: "",
+      placeOfBirth: "",
+      DateOfBirth: "",
+    },
+    professionalSummary: null,
+    workExperience: [],
+    skills: [],
+    languages: [],
+    references: [],
+    educations: [],
+    socialProfiles: [],
+  },
+};
+
+const resumeEditorSlice = createSlice({
+  name: "resumeEditor",
+  initialState,
+  reducers: {
+    setResumeEditorLoading(state, action: PayloadAction<boolean>) {
+      state.isLoading = action.payload;
+    },
+    setResumeEditorSyncing(state, action: PayloadAction<boolean>) {
+      state.isSyncing = action.payload;
+    },
+    setResume(state, action: PayloadAction<IResumeData>) {
+      state.resume = action.payload;
+    },
+    setError(state, action: PayloadAction<string | null>) {
+      state.error = action.payload;
+    },
+    setWorkExperience(
+      state,
+      action: PayloadAction<TypeOfSingleEmploymentHistory[]>
+    ) {
+      state.resume.workExperience = action.payload;
+    },
+    setEducation(state, action: PayloadAction<TypeOfSingleEducationHistory[]>) {
+      state.resume.educations = action.payload;
+    },
+    setSkills(state, action: PayloadAction<TypeOfSkill[]>) {
+      state.resume.skills = action.payload;
+    },
+    setLanguage(state, action: PayloadAction<TypeOfLanguage[]>) {
+      state.resume.languages = action.payload;
+    },
+    setReference(state, action: PayloadAction<TypeOfReference[]>) {
+      state.resume.references = action.payload;
+    },
+    setSocialProfile(
+      state,
+      action: PayloadAction<TypeOfSingleSocialWebSite[]>
+    ) {
+      state.resume.socialProfiles = action.payload;
+    },
+  },
+});
+
+export const {
+  setResumeEditorLoading,
+  setResumeEditorSyncing,
+  setResume,
+  setError,
+  setWorkExperience,
+  setEducation,
+  setSkills,
+  setReference,
+  setLanguage,
+  setSocialProfile,
+} = resumeEditorSlice.actions;
+
+export default resumeEditorSlice.reducer;
