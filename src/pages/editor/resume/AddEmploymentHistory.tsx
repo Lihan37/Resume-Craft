ae94df8cb9e1998f934d3d933d0ea940313b2769
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-07 02:59:37 +0600

    Refactor AddEmploymentHistory component to accept 'initialValue' prop
    This commit refactors the AddEmploymentHistory component to accept an 'initialValue' prop, allowing the parent component to provide initial data for employment history items. With this change:
    - Updated the AddEmploymentHistoryProps interface to include the 'initialValue' prop.
    - Modified the component to set its internal state based on the provided 'initialValue' if available.
    - Updated the component's rendering logic to pass the initial values to child components such as AddSingleEmploymentHistory.
    
    This change enhances the reusability and flexibility of the AddEmploymentHistory component by allowing it to be pre-filled with initial employment history data.

--- src/pages/editor/resume/AddEmploymentHistory.tsx
+++ src/pages/editor/resume/AddEmploymentHistory.tsx
@@ -2,62 +2,69 @@ import React, { useEffect, useState } from "react";
 import AddSingleEmploymentHistory from "./AddSingleEmploymentHistory";
 import { FaPlus } from "react-icons/fa6";
 import createArrayUpToNumber from "../../../utils/createArrayUpToNumber";
-import { TypeOfSingleEmploymentHistory } from "../../../types";
+import { TypeOfSingleEmploymentHistory } from "../../../types/resumeEditor";
 
 interface IAddEmploymentHistory {
   getValue?: (data: TypeOfSingleEmploymentHistory[]) => void;
+  initialValue?: TypeOfSingleEmploymentHistory[];
 }
 
 const AddEmploymentHistory: React.FC<IAddEmploymentHistory> = ({
   getValue = () => {},
+  initialValue,
 }) => {
-  const [addMore, setAddMore] = useState<number>(1);
+  const [addMore, setAddMore] = useState<number>(
+    initialValue && initialValue?.length > 0 ? initialValue?.length : 1
+  );
   const [employmentHistory, setEmploymentHistory] = useState<
     TypeOfSingleEmploymentHistory[]
-  >([]);
+  >(initialValue || []);
 
   const handleSingleHistory = (data: TypeOfSingleEmploymentHistory) => {
-    // add new history
-    const isAlreadyExist = employmentHistory?.find(
-      (item) => item._id === data._id
-    );
-
-    if (!isAlreadyExist) {
-      const filterData = employmentHistory.filter(
-        (item) => item._id !== data._id
-      );
-      return setEmploymentHistory([...filterData, data]);
-    }
-
-    // edit old history
-    const updateHistory = employmentHistory?.map((item) => {
+    const updatedHistory = employmentHistory.map((item) => {
       if (item._id === data._id) {
-        item.jobTitle = data.jobTitle;
-        item.city = data.city;
-        item.description = data.description;
-        item.startMontYear = data.startMontYear;
-        item.endMontYear = data.endMontYear;
-        item.employer = data.employer;
+        return {
+          ...item,
+          jobTitle: data.jobTitle,
+          city: data.city,
+          description: data.description,
+          startMontYear: data.startMontYear,
+          endMontYear: data.endMontYear,
+          employer: data.employer,
+        };
       }
       return item;
     });
-    setEmploymentHistory(updateHistory);
+    const isAlreadyExist = updatedHistory.find((item) => item._id === data._id);
+
+    if (!isAlreadyExist) {
+      setEmploymentHistory([...updatedHistory, data]);
+    } else {
+      setEmploymentHistory(updatedHistory);
+    }
   };
 
   useEffect(() => {
-    getValue(employmentHistory);
+    if (typeof getValue === "function") {
+      getValue(employmentHistory);
+    }
   }, [employmentHistory]);
 
-  // console.log("employmentHistory 2", employmentHistory);
   return (
     <div className=" space-y-3 bg-white overflow-hidden">
-      {createArrayUpToNumber(addMore).map((item) => (
-        <AddSingleEmploymentHistory
-          id={item}
-          getValue={handleSingleHistory}
-          key={item}
-        />
-      ))}
+      {createArrayUpToNumber(addMore).map((item: number) => {
+        const initialSingleData = employmentHistory?.find(
+          (i) => i._id === item
+        );
+        return (
+          <AddSingleEmploymentHistory
+            id={item}
+            getValue={handleSingleHistory}
+            key={item}
+            initialValue={initialSingleData}
+          />
+        );
+      })}
       <button
         onClick={() => setAddMore((prev) => prev + 1)}
         className="pb-5 px-3 font-semibold hover:text-blue-700 py-1 duration-300 transition-colors  text-c-primary flex justify-start items-center gap-4 ">
