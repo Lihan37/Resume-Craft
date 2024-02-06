e6abcbced7180ce473a047024716cb967aade870
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-07 03:12:27 +0600

    Refactor AddSocialWebSite component to accept 'initialValue' prop
    This commit modifies the AddSocialWebSite component to accept an 'initialValue' prop, allowing the parent component to provide initial data for social website links. With this change:
    - Updated the AddSocialWebSiteProps interface to include the 'initialValue' prop.
    - Adjusted the component to set its internal state based on the provided 'initialValue' if available.
    - Enhanced the rendering logic to display the initial values for social website links.
    
    This enhancement improves the flexibility and reusability of the AddSocialWebSite component by enabling it to be pre-populated with initial social website link data.

--- src/pages/editor/resume/AddSocialWebSite.tsx
+++ src/pages/editor/resume/AddSocialWebSite.tsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 import createArrayUpToNumber from "../../../utils/createArrayUpToNumber";
-import { TypeOfSingleSocialWebSite } from "../../../types";
+import { TypeOfSingleSocialWebSite } from "../../../types/resumeEditor";
 import AddSingeSocialWebSite from "./AddSingeSocialWebSite";
 
 interface IAddSocialWebSite {
   getValue?: (data: TypeOfSingleSocialWebSite[]) => void;
+  initialValue?: TypeOfSingleSocialWebSite[];
 }
 
 const AddSocialWebSite: React.FC<IAddSocialWebSite> = ({
   getValue = () => {},
+  initialValue,
 }) => {
-  const [addMore, setAddMore] = useState<number>(1);
+  const [addMore, setAddMore] = useState<number>(
+    initialValue && initialValue?.length > 0 ? initialValue?.length : 1
+  );
   const [socialWebSite, setSocialWebSite] = useState<
     TypeOfSingleSocialWebSite[]
-  >([]);
+  >(initialValue || []);
 
   const handleSingleHistory = (data: TypeOfSingleSocialWebSite) => {
     // add new history
@@ -28,8 +32,11 @@ const AddSocialWebSite: React.FC<IAddSocialWebSite> = ({
     // edit old history
     const updateHistory = socialWebSite?.map((item) => {
       if (item._id === data._id) {
-        item.label = data.label;
-        item.link = data.link;
+        return {
+          ...item,
+          label: data.label,
+          link: data.link,
+        };
       }
       return item;
     });
@@ -37,18 +44,25 @@ const AddSocialWebSite: React.FC<IAddSocialWebSite> = ({
   };
 
   useEffect(() => {
-    getValue(socialWebSite);
+    if (typeof getValue === "function") {
+      getValue(socialWebSite);
+    }
   }, [socialWebSite]);
 
   return (
     <div className=" space-y-3 bg-white overflow-hidden">
-      {createArrayUpToNumber(addMore).map((item) => (
-        <AddSingeSocialWebSite
-          id={item}
-          getValue={handleSingleHistory}
-          key={item}
-        />
-      ))}
+      {createArrayUpToNumber(addMore).map((item) => {
+        const initialSingleData = socialWebSite?.find((i) => i._id === item);
+
+        return (
+          <AddSingeSocialWebSite
+            id={item}
+            getValue={handleSingleHistory}
+            key={item}
+            initialValue={initialSingleData}
+          />
+        );
+      })}
       <button
         onClick={() => setAddMore((prev) => prev + 1)}
         className=" px-3 font-semibold hover:text-blue-700 py-1 duration-300 transition-colors  text-c-primary flex justify-start items-center gap-4">
