52cbaee91ef4e988f0cfb230905fbc81359388a6
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-06 19:31:14 +0600

    Add FontColorSelect component for selecting font color
    This commit introduces the FontColorSelect component, which provides options for selecting font color. It includes both a color picker input and a text input for manual color entry.
    
    The component accepts a prop isSelectedValue to indicate the initially selected font color and a callback function getValue to provide the selected color value to the parent component.
    
    The color state is managed using React useState hook, and useEffect hook is used to trigger the getValue callback when the color changes.
    
    The component is styled using Tailwind CSS utility classes to achieve a consistent and visually appealing design.
    
    This component can be easily integrated into React applications to enable users to customize font color preferences.

---
+++ src/pages/editor/resume/FontColorSelect.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+interface IFontColorSelect {
+  isSelectedValue: string;
+  getValue?: (data: string) => void;
+}
+
+const FontColorSelect: React.FC<IFontColorSelect> = ({
+  isSelectedValue = "#00000",
+  getValue = () => {},
+}) => {
+  const [color, setColor] = useState<string>(isSelectedValue);
+
+  useEffect(() => {
+    getValue(color);
+  }, [color]);
+  return (
+    <div className="font-semibold text-lg gap-5 p-2 px-4 border-[1.8px] rounded-md flex justify-start items-center">
+      <input
+        className="w-7 rounded-md border-none outline-none cursor-pointer h-7"
+        type="color"
+        onChange={(e) => setColor(e.target.value)}
+        name="color"
+        id="color"
+        value={color}
+      />
+      <input
+        onChange={(e) => setColor(e.target.value)}
+        className=" outline-none"
+        type="text"
+        placeholder="#00000"
+        value={color}
+      />
+    </div>
+  );
+};
+
+export default FontColorSelect;
