4fa11392a57b40c3ba43b829e05c9c81360b9e73
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-07 02:52:46 +0600

    Refactor AccordionHeader component to accept 'tittle' prop
    This commit refactors the AccordionHeader component to accept a 'tittle' prop instead of 'children'. The 'tittle' prop represents the title of the accordion header.
    
    With this change:
    - Updated the AccordionHeaderProps interface to include 'tittle' prop.
    - Updated the component to use the 'tittle' prop instead of 'children' for displaying the header title.
    
    This change provides more clarity and flexibility in how the AccordionHeader component can be used, allowing for a more descriptive prop name.

--- src/components/accordion/AccordionHeader.tsx
+++ src/components/accordion/AccordionHeader.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { FC } from "react";
 import { motion } from "framer-motion";
 import { FaPlus, FaMinus } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,11 +10,11 @@ import { RootState } from "../../app/store";
 import { selectActiveAccordion } from "../../services/accordion/accordionSelector";
 
 interface AccordionHeaderProps {
-  children: ReactNode;
   id: number;
+  tittle?: string;
 }
 
-const AccordionHeader: FC<AccordionHeaderProps> = ({ children, id }) => {
+const AccordionHeader: FC<AccordionHeaderProps> = ({ id, tittle }) => {
   const dispatch = useDispatch();
 
   const isActive = useSelector((state: RootState) =>
@@ -33,7 +33,7 @@ const AccordionHeader: FC<AccordionHeaderProps> = ({ children, id }) => {
     <motion.div
       className={`px-5 cursor-pointer border-t-[1.8px] p-2 py-3 flex justify-between items-center w-full`}
       onClick={onChangeIndex}>
-      <h1 className=" font-semibold text-lg text-c-dark">{children} </h1>
+      <h1 className=" font-semibold text-lg text-c-dark">{tittle} </h1>
       {isActive ? <FaMinus /> : <FaPlus />}
     </motion.div>
   );
