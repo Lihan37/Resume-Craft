2a05b0575b5b4f4c09dada73769e2712c695cb32
bikash-dev <bikash.dev.bk@gmail.com>
2024-02-07 18:00:19 +0600

    sidebar menu added

--- src/pages/dashboard/admin/index.tsx
+++ src/pages/dashboard/admin/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const AdminDashboard: React.FC = () => {
   return (
     <div
-      style={{ height: window.innerHeight - 32 }}
       className=" w-full rounded-xl overflow-hidden h-screen bg-white">
       AdminDashboard
     </div>
