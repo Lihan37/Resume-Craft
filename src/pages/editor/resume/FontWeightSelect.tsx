f5626a5d7483d4dc818e624da5629e3c18b24d4e
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-09 13:07:23 +0600

    Refactored FontWeightSelect component
    - Added defaultProps for isSelectedValue and getValue to improve component flexibility
    - Utilized useEffect hook to synchronize the select state with the isSelectedValue prop
    - Updated logic to handle cases where isSelectedValue is 0 or undefined by defaulting to 200
    - Improved performance by setting default value for isSelectedValue prop and providing fallback values in useEffect and setSelect

--- src/pages/editor/resume/FontWeightSelect.tsx
+++ src/pages/editor/resume/FontWeightSelect.tsx
@@ -10,38 +10,45 @@ const fontFamily = [
   { value: 800, label: "Extrabold" },
 ];
 
-interface IValue {
-  value: number;
-  label: string;
-}
-
 interface IFontWeightSelect {
-  isSelectedValue: IValue;
-  getValue?: (data: IValue) => void;
+  isSelectedValue?: number;
+  getValue?: (data: number) => void;
 }
 
 const FontWeightSelect: React.FC<IFontWeightSelect> = ({
-  isSelectedValue,
+  isSelectedValue = 200,
   getValue = () => {},
 }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [select, setSelect] = useState<IValue>(isSelectedValue);
+  const [select, setSelect] = useState<number>(
+    isSelectedValue === 0 ? 200 : isSelectedValue
+  );
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    getValue(select);
+    setSelect(
+      isSelectedValue === 0 || !isSelectedValue ? 200 : isSelectedValue
+    );
+  }, [isSelectedValue]);
+
+  useEffect(() => {
+    if (typeof getValue === "function" && isSelectedValue !== select) {
+      getValue(select);
+    }
   }, [select]);
 
   useOutsideClick(ref, () => {
     setIsOpen(false);
   });
-
+  const selected = fontFamily.find((i) => i.value === select);
   return (
     <div
       ref={ref}
       onClick={() => setIsOpen((prev) => !prev)}
       className="cursor-pointer relative select-none flex justify-between items-center p-2 border-[1.8px] rounded-md px-4">
-      <span className=" font-semibold text-lg">{select.label}</span>
+      <span style={{ fontWeight: select }} className=" font-semibold text-lg">
+        {selected?.label}
+      </span>
       {isOpen ? (
         <IoIosArrowUp className="text-xl" />
       ) : (
@@ -49,18 +56,24 @@ const FontWeightSelect: React.FC<IFontWeightSelect> = ({
       )}
       {isOpen && (
         <div className=" z-50 rounded-md absolute w-full left-0 right-0 bg-white top-12 shadow-[0_8px_30px_rgb(0,0,0,0.12)] font-semibold">
-          {fontFamily.map((item) => (
-            <h1
-              style={{ fontWeight: item.value }}
-              onClick={() => setSelect(item)}
-              className={`  p-2 px-4 hover:text-c-primary duration-300 transition-colors`}>
-              {item.label}
-            </h1>
-          ))}
+          {fontFamily.map((item) => {
+            const isActive = item.value === select;
+            return (
+              <h1
+                key={item.value}
+                style={{ fontWeight: item.value }}
+                onClick={() => setSelect(item.value)}
+                className={` ${
+                  isActive && "text-c-primary"
+                } p-2 px-4 hover:text-c-primary duration-300 transition-colors`}>
+                {item.label}
+              </h1>
+            );
+          })}
         </div>
       )}
     </div>
   );
 };
 
-export default FontWeightSelect;
+export default React.memo(FontWeightSelect);
