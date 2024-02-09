c997f88e6e5325d5c45fe3fc4b1e9c2fc9d3ca36
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-09 13:04:25 +0600

    Refactored FontAlignSelect component
    - Added defaultProps for isSelectedValue and getValue to improve component flexibility
    - Utilized useEffect hook to synchronize the fontAlign state with the isSelectedValue prop
    - Updated logic to trigger the getValue callback only when fontAlign state changes

--- src/pages/editor/resume/FontAlignSelect.tsx
+++ src/pages/editor/resume/FontAlignSelect.tsx
@@ -7,39 +7,54 @@ import {
 } from "react-icons/fa6";
 
 interface IFontAlignSelect {
-  isSelectedValue: string;
+  isSelectedValue?: string;
   getValue?: (data: string) => void;
 }
 
 const FontAlignSelect: React.FC<IFontAlignSelect> = ({
-  isSelectedValue,
+  isSelectedValue = "start",
   getValue = () => {},
 }) => {
   const [fontAlign, setFontAlign] = useState<string>(isSelectedValue);
 
   useEffect(() => {
-    getValue(fontAlign);
-  }, []);
+    setFontAlign(isSelectedValue);
+  }, [isSelectedValue]);
+
+  useEffect(() => {
+    if (typeof getValue === "function" && isSelectedValue !== fontAlign) {
+      getValue(fontAlign);
+    }
+  }, [fontAlign]);
+
   return (
     <div className="font-semibold px-4 border-[1.8px] rounded-md flex justify-between items-center text-xl">
       <div
-        onClick={() => setFontAlign("text-start")}
-        className="w-full cursor-pointer border-r-[1.8px] p-3 flex justify-center">
+        onClick={() => setFontAlign("start")}
+        className={` ${
+          fontAlign === "start" && "text-c-primary"
+        } w-full cursor-pointer border-r-[1.8px] p-3 flex justify-center`}>
         <FaAlignLeft />
       </div>
       <div
-        onClick={() => setFontAlign("text-end")}
-        className="w-full cursor-pointer border-r-[1.8px] p-3 flex justify-center">
+        onClick={() => setFontAlign("end")}
+        className={` ${
+          fontAlign === "end" && "text-c-primary"
+        } w-full cursor-pointer border-r-[1.8px] p-3 flex justify-center`}>
         <FaAlignRight />
       </div>
       <div
-        onClick={() => setFontAlign("text-center")}
-        className="w-full cursor-pointer border-r-[1.8px] p-3 flex justify-center">
+        onClick={() => setFontAlign("center")}
+        className={` ${
+          fontAlign === "center" && "text-c-primary"
+        } w-full cursor-pointer border-r-[1.8px] p-3 flex justify-center`}>
         <FaAlignCenter />
       </div>
       <div
-        onClick={() => setFontAlign("text-justify")}
-        className="w-full cursor-pointer p-3  flex justify-center">
+        onClick={() => setFontAlign("justify")}
+        className={` ${
+          fontAlign === "justify" && "text-c-primary"
+        } w-full cursor-pointer  border-r-[1.8px] p-3 flex justify-center`}>
         <FaAlignJustify />
       </div>
     </div>
