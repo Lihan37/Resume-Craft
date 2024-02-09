85df40a000f680cce1aa3b7f1f8902bd194c361d
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-09 12:54:20 +0600

    Refactor Editor component and integrate with Redux state
    - Updated imports and selectors to match changes in Redux slice
    - Added conditional rendering based on editor loading state
    - Added useEffect hooks to fetch single resume data and update the resume on changes
    - Updated template rendering logic to account for editor loading state and resume data availability

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -8,20 +8,39 @@ import RightSideBar from "./rightBar/RightSideBar";
 import RightSideBarOptions from "./resume/RightSideBarOptions";
 import { IoSettingsOutline } from "react-icons/io5";
 import { useSelector } from "react-redux";
-import { selectResume } from "../../services/resumeEditor/resumeEditorSelector";
-import resumes, { ResumeTemplatesType } from "../../components/resumeTemplates";
-import { createResumeAndUpdate } from "../../services/resumeEditor/resumeEditorApi";
+import {
+  selectResume,
+  selectResumeEditor,
+} from "../../services/resumeEditor/resumeEditorSelector";
+import resumes, {
+  ResumeTemplatesType,
+} from "../../components/resumeTemplates/template";
+import {
+  createResumeAndUpdate,
+  getSingleResumeData,
+} from "../../services/resumeEditor/resumeEditorApi";
 import { useAppDispatch } from "../../app/store";
+import { useParams } from "react-router-dom";
 
 const Editor: React.FC = () => {
   useTitleSet("Resume Builder");
   const [windowWidth] = useDisplay();
   const resume = useSelector(selectResume);
+  const editor = useSelector(selectResumeEditor);
   const dispatch = useAppDispatch();
+  const param = useParams();
+
+  useEffect(() => {
+    if (param.id && !resume._id) {
+      dispatch(getSingleResumeData(param.id));
+    }
+  }, [param.id]);
 
   useEffect(() => {
     const timeoutId = setTimeout(async () => {
-      dispatch(createResumeAndUpdate(resume));
+      if (param.id === resume._id) {
+        dispatch(createResumeAndUpdate(resume));
+      }
     }, 500);
 
     return () => {
@@ -35,40 +54,45 @@ const Editor: React.FC = () => {
       : window.innerHeight - 88 + "px";
 
   const Template =
-    resumes[resume.templateId as keyof ResumeTemplatesType].template;
+    !editor.isLoading && resumes[resume.templateId as keyof ResumeTemplatesType]
+      ? resumes[resume.templateId as keyof ResumeTemplatesType].template
+      : null;
 
   return (
-    <div
-      style={{ minHeight: hight }}
-      className="flex justify-start items-start">
-      <div className="h-full min-w-[400px] max-w-[400px]  ">
-        <LeftSideBar
-          editor={<LeftSideBarOptions />}
-          templates={<ResumeTemplates />}
-        />
-      </div>
+    !editor.isLoading && (
       <div
-        style={{ height: hight }}
-        id="resumeEditorDashboard"
-        className="w-full flex overflow-auto justify-center items-center bg-zinc-100 ">
-        {/* <Toronto resume={resume} /> */}
-        <Template resume={resume} />
-      </div>
+        style={{ minHeight: hight }}
+        className="flex justify-start items-start">
+        <div className="h-full min-w-[400px] max-w-[400px]  ">
+          <LeftSideBar
+            editor={<LeftSideBarOptions />}
+            templates={<ResumeTemplates />}
+          />
+        </div>
 
-      <div className="2xl:hidden fixed right-0 top-56 py-2 px-6 hover:bg-white hover:text-c-primary duration-300 transition-colors cursor-pointer text-white rounded-l-full bg-c-primary">
-        <IoSettingsOutline className="animate-spin text-4xl" />
+        {/* Editor Dashboard  */}
+        <div
+          style={{ height: hight }}
+          id="resumeEditorDashboard"
+          className="w-full flex overflow-auto justify-center items-center bg-zinc-100 ">
+          {Template && <Template resume={resume} />}
+        </div>
+
+        <div className="2xl:hidden fixed right-0 top-56 py-2 px-6 hover:bg-white hover:text-c-primary duration-300 transition-colors cursor-pointer text-white rounded-l-full bg-c-primary">
+          <IoSettingsOutline className="animate-spin text-4xl" />
+        </div>
+        <div
+          className={`${
+            windowWidth < 1590
+              ? "fixed top-[88px] xl:top-[98px] z-50 -right-[50%]"
+              : null
+          }`}>
+          <RightSideBar>
+            <RightSideBarOptions />
+          </RightSideBar>
+        </div>
       </div>
-      <div
-        className={`${
-          windowWidth < 1590
-            ? "fixed top-[88px] xl:top-[98px] z-50 -right-[50%]"
-            : null
-        }`}>
-        <RightSideBar>
-          <RightSideBarOptions />
-        </RightSideBar>
-      </div>
-    </div>
+    )
   );
 };
 
