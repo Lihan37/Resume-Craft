cd1e4f8acf2c00f2a116c7be906f0d6aa2dc13a9
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-09 13:08:08 +0600

    Refactored PersonaData component
    - Added support for handling focus events on input fields and tracking the focused input using state
    - Introduced new props getFocusedInputValue and initialFocusedValue to manage focused input state
    - Updated the component to pass the focused input name to the parent component when it gains focus
    - Improved component flexibility by allowing customization of initial focused input value and handling focus events

--- src/pages/editor/resume/PersonaData.tsx
+++ src/pages/editor/resume/PersonaData.tsx
@@ -5,6 +5,7 @@ import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 import { IResumePersonalInfo } from "../../../types/resumeEditor";
 
 const initialState = {
+  _id: "",
   jobTitle: "",
   firstName: "",
   lastName: "",
@@ -23,15 +24,32 @@ const initialState = {
 interface IPersonaData {
   getValue?: (data: IResumePersonalInfo) => void;
   initialValue?: IResumePersonalInfo;
+  getFocusedInputValue?: (data: string) => void;
+  initialFocusedValue?: string;
 }
 const PersonaData: React.FC<IPersonaData> = ({
   getValue = () => {},
+  getFocusedInputValue = () => {},
   initialValue,
+  initialFocusedValue,
 }) => {
   const [isActive, setIsActive] = useState(false);
   const [state, setState] = useState<IResumePersonalInfo>(
     initialValue || initialState
   );
+  const [focusedInput, setFocusedInput] = useState<string>(
+    initialFocusedValue || ""
+  );
+
+  useEffect(() => {
+    if (
+      getFocusedInputValue &&
+      typeof getFocusedInputValue === "function" &&
+      focusedInput !== initialFocusedValue
+    ) {
+      getFocusedInputValue(focusedInput);
+    }
+  }, [focusedInput]);
 
   useEffect(() => {
     if (getValue && typeof getValue === "function") {
@@ -44,6 +62,10 @@ const PersonaData: React.FC<IPersonaData> = ({
     setState((prev: IResumePersonalInfo) => ({ ...prev, [name]: value }));
   };
 
+  const handleInputFocus = (inputName: string) => {
+    setFocusedInput(inputName);
+  };
+
   return (
     <div className=" space-y-3 py-5 px-5 ">
       <InputText
@@ -51,36 +73,42 @@ const PersonaData: React.FC<IPersonaData> = ({
         name="jobTitle"
         placeholder="Job Title"
         onChange={handleInputValue}
+        onFocus={() => handleInputFocus("jobTitle")}
       />
       <InputText
         value={state.firstName}
         name="firstName"
         placeholder="First Name"
         onChange={handleInputValue}
+        onFocus={() => handleInputFocus("firstName")}
       />
       <InputText
         value={state.lastName}
         placeholder="Last Name"
         name="lastName"
         onChange={handleInputValue}
+        onFocus={() => handleInputFocus("lastName")}
       />
       <InputText
         value={state.email}
         onChange={handleInputValue}
         name="email"
         placeholder="Email Address"
+        onFocus={() => handleInputFocus("email")}
       />
       <InputText
         value={state.phoneNumber}
         onChange={handleInputValue}
         name="phoneNumber"
         placeholder="Phone Number"
+        onFocus={() => handleInputFocus("phoneNumber")}
       />
       <InputText
         value={state.country}
         onChange={handleInputValue}
         name="country"
         placeholder="Your Country"
+        onFocus={() => handleInputFocus("country")}
       />
 
       <AnimatePresence initial={false}>
@@ -96,42 +124,49 @@ const PersonaData: React.FC<IPersonaData> = ({
                 onChange={handleInputValue}
                 name="city"
                 placeholder="Your City"
+                onFocus={() => handleInputFocus("city")}
               />
               <InputText
                 value={state.address}
                 onChange={handleInputValue}
                 name="address"
                 placeholder="Your Address"
+                onFocus={() => handleInputFocus("address")}
               />
               <InputText
                 value={state.postalCode}
                 onChange={handleInputValue}
                 name="postalCode"
                 placeholder="Postal Code"
+                onFocus={() => handleInputFocus("postalCode")}
               />
               <InputText
                 value={state.drivingLicense}
                 onChange={handleInputValue}
                 name="drivingLicense"
                 placeholder="Driving License"
+                onFocus={() => handleInputFocus("drivingLicense")}
               />
               <InputText
                 value={state.nationality}
                 onChange={handleInputValue}
                 name="nationality"
                 placeholder="Nationality"
+                onFocus={() => handleInputFocus("nationality")}
               />
               <InputText
                 value={state.placeOfBirth}
                 onChange={handleInputValue}
                 name="placeOfBirth"
                 placeholder="Place Of Birth"
+                onFocus={() => handleInputFocus("placeOfBirth")}
               />
               <InputText
                 value={state.DateOfBirth}
                 onChange={handleInputValue}
                 name="DateOfBirth"
                 placeholder="Date Of Birth"
+                onFocus={() => handleInputFocus("DateOfBirth")}
               />
             </div>
           </motion.div>
