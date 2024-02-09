141c92e09b8f1464878ac501c4a7074f8e464958
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-09 12:50:19 +0600

    Update async thunk functions for resume operations
    - Updated createResumeAndUpdate thunk to remove console log and improve error handling
    - Added getSingleResumeData thunk for fetching single resume data

--- src/services/resumeEditor/resumeEditorApi.ts
+++ src/services/resumeEditor/resumeEditorApi.ts
@@ -4,7 +4,6 @@ import { IResumeData } from "./resumeEditorSlice";
 export const createResumeAndUpdate = createAsyncThunk(
   "resumeEditor/CreateResumeAndUpdate",
   async (resumeData: IResumeData, { rejectWithValue }) => {
-    console.log("resumeData", resumeData);
     try {
       const response = await fetch("http://localhost:8000/resume/v1/create", {
         method: "POST",
@@ -13,7 +12,24 @@ export const createResumeAndUpdate = createAsyncThunk(
         },
         body: JSON.stringify(resumeData),
       });
+      return await response.json();
+    } catch (error) {
+      return rejectWithValue(
+        (error as Error).message || "An error occurred during login"
+      );
+    }
+  }
+);
+
+export const getSingleResumeData = createAsyncThunk(
+  "resumeEditor/getSingleResumeData",
+  async (id: string, { rejectWithValue }) => {
+    try {
+      const response = await fetch(`http://localhost:8000/resume/v1/${id}`, {
+        method: "GET",
+      });
       const data = await response.json();
+
       return data;
     } catch (error) {
       return rejectWithValue(
