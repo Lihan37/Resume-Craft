9eae11744c21b668be74cbb4a1e742f3988b716c
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-09 13:05:47 +0600

    Refactored FontFamilySelect component
    - Added defaultProps for isSelectedValue and getValue to improve component flexibility
    - Utilized useEffect hook to synchronize the select state with the isSelectedValue prop
    - Updated logic to trigger the getValue callback only when select state changes
    - Improved performance by setting keys for mapped elements and optimizing rendering by directly referencing selected label

--- src/pages/editor/resume/FontFamilySelect.tsx
+++ src/pages/editor/resume/FontFamilySelect.tsx
@@ -12,38 +12,42 @@ const fontFamily = [
   { value: "'Titillium Web', sans-serif", label: "Titillium" },
 ];
 
-interface IValue {
-  value: string;
-  label: string;
-}
-
 interface IFontFamilySelect {
-  isSelectedValue: IValue;
-  getValue?: (data: IValue) => void;
+  isSelectedValue?: string;
+  getValue?: (data: string) => void;
 }
 
 const FontFamilySelect: React.FC<IFontFamilySelect> = ({
-  isSelectedValue,
+  isSelectedValue = "'Lato', sans-serif",
   getValue = () => {},
 }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [select, setSelect] = useState<IValue>(isSelectedValue);
+  const [select, setSelect] = useState<string>(isSelectedValue);
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    getValue(select);
+    setSelect(isSelectedValue);
+  }, [isSelectedValue]);
+
+  useEffect(() => {
+    if (typeof getValue === "function" && isSelectedValue !== select) {
+      getValue(select);
+    }
   }, [select]);
 
   useOutsideClick(ref, () => {
     setIsOpen(false);
   });
 
+  const selected = fontFamily.find((i) => i.value === select);
   return (
     <div
       ref={ref}
       onClick={() => setIsOpen((prev) => !prev)}
       className="cursor-pointer relative select-none flex justify-between items-center p-2 border-[1.8px] rounded-md px-4">
-      <span className=" font-semibold text-lg">{select.label}</span>
+      <span style={{ fontFamily: select }} className=" font-semibold text-lg">
+        {selected?.label}
+      </span>
       {isOpen ? (
         <IoIosArrowUp className="text-xl" />
       ) : (
@@ -51,18 +55,24 @@ const FontFamilySelect: React.FC<IFontFamilySelect> = ({
       )}
       {isOpen && (
         <div className=" z-50 rounded-md absolute w-full left-0 right-0 bg-white top-12 shadow-[0_8px_30px_rgb(0,0,0,0.12)] font-semibold">
-          {fontFamily.map((item) => (
-            <h1
-              style={{ fontFamily: item.value }}
-              onClick={() => setSelect(item)}
-              className={`  p-2 px-4 hover:text-c-primary duration-300 transition-colors`}>
-              {item.label}
-            </h1>
-          ))}
+          {fontFamily.map((item) => {
+            const isActive = item.value === select;
+            return (
+              <h1
+                key={item.value}
+                style={{ fontFamily: item.value }}
+                onClick={() => setSelect(item.value)}
+                className={`${
+                  isActive && "text-c-primary"
+                }  p-2 px-4 hover:text-c-primary duration-300 transition-colors`}>
+                {item.label}
+              </h1>
+            );
+          })}
         </div>
       )}
     </div>
   );
 };
 
-export default FontFamilySelect;
+export default React.memo(FontFamilySelect);
