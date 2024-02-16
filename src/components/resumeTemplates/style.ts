06f19a522802d979cefcd5ce5309488c0e8eb69e
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-16 16:46:26 +0600

    Merge

--- src/components/resumeTemplates/style.ts
+++ src/components/resumeTemplates/style.ts
@@ -1,9 +1,11 @@
 import styleStockholm from "./stockholm/StockholmStyle";
+import styleSydney from "./sydney/SydneyStyle";
 import styleToronto from "./toronto/TorontoStyle";
 
 const resumeStyle = {
   toronto01: { id: "toronto01", style: styleToronto },
   stockholm01: { id: "stockholm01", style: styleStockholm },
+  sydney01: { id: "sydney01", style: styleSydney },
 };
 
 export default resumeStyle;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,7 +23,6 @@ import { PDFViewer } from "@react-pdf/renderer";
 import ViennaPDF from "../components/resumeTemplates/vienna/ViennaPDF";
 import Madrid from "../components/resumeTemplates/madrid/Madrid";
 import CoverLetter from "../pages/editor/CoverLetter";
-import Sydney from "../components/resumeTemplates/sydney/Sydney";
 
 const router = createBrowserRouter([
   {
@@ -123,10 +122,6 @@ const router = createBrowserRouter([
   {
     path: "/madrid",
     element: <Madrid />,
-  },
-  {
-    path: "/sweeny",
-    element: <Sydney></Sydney>,
   },
   {
     path: "/demopdf",
