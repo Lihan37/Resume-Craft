525e110f16780f84037a4fe08ccb21644f2f1f0a
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-16 13:13:01 +0600

    feat: Implement LeftSideBarOptions component for cover letter editor
    This commit adds the LeftSideBarOptions component, which contains input fields for entering personal details, employer details, and letter details in the cover letter editor. The component dispatches actions to update the corresponding values in the Redux store upon user input.
    
    Changes Made:
    - Added LeftSideBarOptions component for cover letter editor
    - Implemented input fields for personal details, employer details, and letter details
    - Dispatched actions to update values in the Redux store upon user input

--- src/pages/editor/coverletter/LeftSideBarOptions.tsx
+++ src/pages/editor/coverletter/LeftSideBarOptions.tsx
@@ -1,22 +1,91 @@
 import React from "react";
 import InputText from "../../../components/common/InputText";
+import { useDispatch } from "react-redux";
+import { setFocus } from "../../../services/generalEditor/generalEditorSlice";
+import { setValue } from "../../../services/coverletterEditor/coverletterEditorSlice";
 
 const LeftSideBarOptions: React.FC = () => {
+  const dispatch = useDispatch();
   return (
     <div>
       <div className="px-5 text-c-dark space-y-4">
         <h1 className=" font-semibold text-xl">Personal Details :</h1>
-        <InputText placeholder="Full Name" />
-        <InputText placeholder="Job Title" />
-        <InputText placeholder="Address" />
-        <InputText placeholder="Email" />
-        <InputText placeholder="Phone Number" />
+        <InputText
+          onFocus={() => {
+            dispatch(setFocus({ focusInput: "fullName" }));
+          }}
+          onChange={(e) => {
+            dispatch(setValue({ name: "fullName", value: e.target.value }));
+          }}
+          placeholder="Full Name"
+        />
+        <InputText
+          onFocus={() => {
+            dispatch(setFocus({ focusInput: "JobTitle" }));
+          }}
+          onChange={(e) => {
+            dispatch(setValue({ name: "JobTitle", value: e.target.value }));
+          }}
+          placeholder="Job Title"
+        />
+        <InputText
+          onFocus={() => {
+            dispatch(setFocus({ focusInput: "address" }));
+          }}
+          onChange={(e) => {
+            dispatch(setValue({ name: "address", value: e.target.value }));
+          }}
+          placeholder="Address"
+        />
+        <InputText
+          onFocus={() => {
+            dispatch(setFocus({ focusInput: "email" }));
+          }}
+          onChange={(e) => {
+            dispatch(setValue({ name: "email", value: e.target.value }));
+          }}
+          placeholder="Email"
+        />
+        <InputText
+          onFocus={() => {
+            dispatch(setFocus({ focusInput: "phoneNumber" }));
+          }}
+          onChange={(e) => {
+            dispatch(setValue({ name: "phoneNumber", value: e.target.value }));
+          }}
+          placeholder="Phone Number"
+        />
 
         <h1 className=" font-semibold text-xl">Employer Details :</h1>
-        <InputText placeholder="Company Name" />
-        <InputText placeholder="Hiring Manager Name" />
+        <InputText
+          onFocus={() => {
+            dispatch(setFocus({ focusInput: "companyName" }));
+          }}
+          onChange={(e) => {
+            dispatch(setValue({ name: "companyName", value: e.target.value }));
+          }}
+          placeholder="Company Name"
+        />
+        <InputText
+          onFocus={() => {
+            dispatch(setFocus({ focusInput: "managerName" }));
+          }}
+          onChange={(e) => {
+            dispatch(setValue({ name: "managerName", value: e.target.value }));
+          }}
+          placeholder="Hiring Manager Name"
+        />
         <h1 className=" font-semibold text-xl">Letter Details :</h1>
-        <InputText textarea={true} />
+        <InputText
+          onFocus={() => {
+            dispatch(setFocus({ focusInput: "details" }));
+          }}
+          onChange={(e) => {
+            dispatch(setValue({ name: "details", value: e.target.value }));
+          }}
+          textarea={true}
+          placeholder="Details..."
+        />
       </div>
     </div>
   );
