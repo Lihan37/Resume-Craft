2164024f4f5aa60686737e04162ea60a108d0ab2
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-16 13:12:23 +0600

    feat: Implement RightSideBarOptions component for cover letter editor
    This commit introduces the RightSideBarOptions component, which provides various options for customizing the style of a cover letter in the editor. The component includes selectors for theme, font family, font weight, font color, font size, and text alignment. It dispatches actions to update the corresponding style properties in the Redux store upon user interaction.
    
    Changes Made:
    - Added RightSideBarOptions component for cover letter editor
    - Implemented selectors for theme, font family, font weight, font color, font size, and text alignment
    - Dispatched actions to update style properties in the Redux store

--- src/pages/editor/coverletter/RightSideBarOptions.tsx
+++ src/pages/editor/coverletter/RightSideBarOptions.tsx
@@ -6,22 +6,122 @@ import FontColorSelect from "../common/FontColorSelect";
 import FontSizeSelect from "../common/FontSizeSelect";
 import FontAlignSelect from "../common/FontAlignSelect";
 import ContentHightWidth from "../common/ContentHightWidth";
+import { useDispatch, useSelector } from "react-redux";
+import { selectFocusInput } from "../../../services/generalEditor/generalEditorSelector";
+import { RootState } from "../../../app/store";
+import { selectStyleCoverLetter } from "../../../services/coverletterEditor/styleSelector";
+import {
+  ICoverLetterStyle,
+  changeSize,
+  changeStyleCovetLetter,
+  changeTheme,
+} from "../../../services/coverletterEditor/coverletterEditorSlice";
+import { TypeOfStyleText } from "../../../types/editor";
+import {
+  selectCoverLetterSize,
+  selectCoverLetterTheme,
+  selectCoverLetterThemeOption,
+} from "../../../services/coverletterEditor/coverletterEditorSelector";
 
 const RightSideBarOptions: React.FC = () => {
+  const focusInput = useSelector(selectFocusInput);
+  const size = useSelector(selectCoverLetterSize);
+  const theme = useSelector(selectCoverLetterTheme);
+  const themeOptions = useSelector(selectCoverLetterThemeOption);
+  const dispatch = useDispatch();
+
+  const style = useSelector((state: RootState) =>
+    selectStyleCoverLetter(state, focusInput as keyof ICoverLetterStyle)
+  );
+
   return (
     <div className="space-y-10 text-c-dark ">
-      <SelectTheme />
+      <SelectTheme
+        selectedValue={theme}
+        values={themeOptions}
+        getValue={(data: string) => {
+          dispatch(changeTheme(data));
+        }}
+      />
       <ContentHightWidth
-        isSelectedValue={{ height: "100px", width: "600px" }}
+        isSelectedValue={size}
+        getValue={(data) => {
+          dispatch(changeSize(data));
+        }}
       />
       <div className=" space-y-2">
         <h1 className=" font-semibold text-2xl capitalize ">Text</h1>
         <div className=" space-y-5">
-          <FontFamilySelect />
-          <FontWeightSelect />
-          <FontColorSelect />
-          <FontSizeSelect />
-          <FontAlignSelect />
+          <FontFamilySelect
+            isSelectedValue={style?.fontFamily}
+            getValue={(data: string) => {
+              dispatch(
+                changeStyleCovetLetter({
+                  fieldName: focusInput as keyof ICoverLetterStyle,
+                  value: {
+                    ...style,
+                    fontFamily: data,
+                  } as TypeOfStyleText,
+                })
+              );
+            }}
+          />
+          <FontWeightSelect
+            isSelectedValue={style?.fontWeight}
+            getValue={(data: number) => {
+              dispatch(
+                changeStyleCovetLetter({
+                  fieldName: focusInput as keyof ICoverLetterStyle,
+                  value: {
+                    ...style,
+                    fontWeight: data,
+                  } as TypeOfStyleText,
+                })
+              );
+            }}
+          />
+          <FontColorSelect
+            isSelectedValue={style?.color}
+            getValue={(data: string) => {
+              dispatch(
+                changeStyleCovetLetter({
+                  fieldName: focusInput as keyof ICoverLetterStyle,
+                  value: {
+                    ...style,
+                    color: data,
+                  } as TypeOfStyleText,
+                })
+              );
+            }}
+          />
+          <FontSizeSelect
+            isSelectedValue={style?.fontSize}
+            getValue={(data: string) => {
+              dispatch(
+                changeStyleCovetLetter({
+                  fieldName: focusInput as keyof ICoverLetterStyle,
+                  value: {
+                    ...style,
+                    fontSize: data,
+                  } as TypeOfStyleText,
+                })
+              );
+            }}
+          />
+          <FontAlignSelect
+            isSelectedValue={style?.textAlign}
+            getValue={(data: string) => {
+              dispatch(
+                changeStyleCovetLetter({
+                  fieldName: focusInput as keyof ICoverLetterStyle,
+                  value: {
+                    ...style,
+                    textAlign: data,
+                  } as TypeOfStyleText,
+                })
+              );
+            }}
+          />
         </div>
       </div>
     </div>
