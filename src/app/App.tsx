7874015df4b86e794c1d1fe51f01df2afe645abb
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-21 18:48:55 +0600

    Update App component to handle loading state during token refresh
    - Add loading state management to display 'Loading...' while refreshing token
    - Improve error handling in case of fetch failures

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "../routes";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,15 +8,22 @@ const baseUrl = import.meta.env.VITE_BASE_URL_API;
 
 const App: React.FC = () => {
   const user = useSelector(selectUser);
+  const [loading, setLoading] = useState<boolean>(true);
   const dispatch = useDispatch();
+  // console.log(loading);
 
   const refreshAccessToken = async () => {
     try {
+      setLoading(true);
       const response = await fetch(`${baseUrl}/auth/v1/refresh`, {
         method: "GET",
         credentials: "include",
       });
       const data = await response.json();
+      console.log(data);
+      if (data.user || data.message) {
+        setLoading(false);
+      }
       dispatch(setUser({ user: data.user, accessToken: data.accessToken }));
     } catch (error) {
       console.log(error);
@@ -35,7 +42,7 @@ const App: React.FC = () => {
     return () => clearInterval(tokenRefreshInterval);
   }, [user]);
 
-  return <RouterProvider router={router} />;
+  return loading ? <h1>Loading....</h1> : <RouterProvider router={router} />;
 };
 
 export default App;
