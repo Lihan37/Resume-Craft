d43e2430fc0b653d361ef9d5b54688bbfa9c78b0
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-26 23:27:01 +0600

    Refactor CreateBlog component to include image upload, form submission, and error handling
    This commit enhances the CreateBlog component to provide a more comprehensive blogging experience. The following changes have been made:
    
    - Implemented image upload functionality: Users can now select an image file (JPEG, JPG, or PNG) to accompany their blog post. The selected image is displayed in the preview section.
    - Added form submission handling: Upon form submission, the component validates the input fields (title, content, and tags) and handles the submission process. Invalid input or failed submission triggers appropriate error messages.
    - Integrated error handling with Swal: Error messages are displayed using the Swal library for a more user-friendly experience. Error messages include notifications for invalid input and submission failures.
    - Updated state management: State is now managed using the useState hook to track the title, content, selected image file, loading state, and tags.
    
    These changes improve the usability and functionality of the CreateBlog component, making it easier for users to create and publish engaging blog posts.

--- src/pages/dashboard/admin/blog/CreateBlog.tsx
+++ src/pages/dashboard/admin/blog/CreateBlog.tsx
@@ -5,11 +5,33 @@ import InputTextEditor from "../../../../components/common/InputTextEditor";
 import Button from "../../../../components/common/Button";
 import { BiLoaderAlt } from "react-icons/bi";
 import { TagsInput } from "react-tag-input-component";
+import Swal from "sweetalert2";
+const baseUrl = import.meta.env.VITE_BASE_URL_API;
+const allowedFileTypes = ["image/jpg", "image/jpeg", "image/png"];
+
+interface IState {
+  title: string;
+  content: string;
+  image: {
+    public_id: string;
+    url: string;
+  };
+  tags: string[];
+}
+const initialState = {
+  title: "",
+  content: "",
+  image: {
+    public_id: "",
+    url: "",
+  },
+  tags: ["News"],
+};
 
 const CreateBlog: FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const [tags, setTags] = useState<string[]>(["News"]);
+  const [state, setState] = useState<IState>(initialState);
 
   useTitleSet("Create Blog");
   const inputRef = useRef<HTMLInputElement>(null);
@@ -19,16 +41,98 @@ const CreateBlog: FC = () => {
       inputRef.current?.click();
     }
   };
+
+  const imageUpload = async () => {
+    if (selectedFile && allowedFileTypes.includes(selectedFile?.type)) {
+      try {
+        const formData = new FormData();
+        formData.append("blogImage", selectedFile);
+
+        const response = await fetch(`${baseUrl}/blog/v1/imageUpload`, {
+          method: "POST",
+          credentials: "include",
+          body: formData,
+        });
+        const data = await response.json();
+        return data.image;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      if (
+        !selectedFile ||
+        !state.title ||
+        !state.content ||
+        state.tags.length === 0
+      ) {
+        setLoading(false);
+        Swal.fire({
+          icon: "error",
+          text: "Invalid input!",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+        return;
+      }
+      const { public_id, url } = await imageUpload();
+      if (public_id && url) {
+        const blogData = { ...state, image: { public_id, url } };
+        const response = await fetch(`${baseUrl}/blog/v1/createBlog`, {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(blogData),
+        });
+        const data = await response.json();
+        if (!data.success) {
+          Swal.fire({
+            icon: "error",
+            text: data.message,
+            showConfirmButton: false,
+            timer: 1000,
+          });
+          setLoading(false);
+        }
+        if (data.success) {
+          setLoading(false);
+          setState(initialState);
+          setSelectedFile(null);
+          Swal.fire({
+            icon: "success",
+            text: "Successfully Create Blog",
+            showConfirmButton: false,
+            timer: 1000,
+          });
+        }
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
   return (
     <div
       style={{ height: window.innerHeight }}
       className="p-4 overflow-scroll pb-20">
       <h1 className="text-4xl text-c-dark-light font-bold ">Create Blog</h1>
-      <div className="  space-y-4 mt-10">
+      <form onSubmit={handleSubmit} className="  space-y-4 mt-10">
         <input
           type="text"
           className="w-full text-3xl px-4 py-3 rounded-md outline-none focus:border-c-primary  border-[1.8px] placeholder:font-semibold placeholder:text-gray-400 placeholder:text-3xl text-c-dark"
           placeholder=" Title.."
+          value={state.title}
+          onChange={(e) =>
+            setState((prv) => ({ ...prv, title: e.target.value }))
+          }
         />
         <div
           onClick={handleImageSelect}
@@ -55,10 +159,18 @@ const CreateBlog: FC = () => {
             ref={inputRef}
           />
         </div>
-        <InputTextEditor height="400px" />
+        <InputTextEditor
+          height="400px"
+          initialValue={state.content}
+          getValue={(data: string) => {
+            setState((prv) => ({ ...prv, content: data }));
+          }}
+        />
         <TagsInput
-          value={tags}
-          onChange={setTags}
+          value={state.tags}
+          onChange={(data) => {
+            setState((prv) => ({ ...prv, tags: data }));
+          }}
           name="tags"
           placeHolder="blogs tags"
         />
@@ -70,7 +182,7 @@ const CreateBlog: FC = () => {
             </div>
           </Button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
