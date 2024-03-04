98507914d867de167a081613e6823fea01553099
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-03-05 01:50:26 +0600

    Add Pricing component for displaying pricing data
    This commit introduces a new React component called Pricing, which displays pricing information retrieved from the API. The component fetches pricing data using the useGetAllPriceQuery hook from the API service. It renders a table with columns for monthly and yearly pricing, status, and actions. Each pricing entry is displayed using the SinglePrice component.

---
+++ src/pages/dashboard/admin/payment/Pricing.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import SinglePrice from "./SinglePrice";
+import { useGetAllPriceQuery } from "../../../../services/api/api";
+import Create from "./Create";
+
+const Pricing: React.FC = () => {
+  const { data, isLoading } = useGetAllPriceQuery();
+
+  return (
+    <div className="p-10  pb-20">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl text-c-dark-light font-bold ">Pricing</h1>;
+        <Create />
+      </div>
+      <div className="p-6 overflow-scroll h-[800px] ">
+        <table className="mt-4 w-full min-w-max table-auto text-left ">
+          <thead>
+            <tr>
+              <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                  Monthly
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="h-4 w-4">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"></path>
+                  </svg>
+                </p>
+              </th>
+              <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                  Yearly
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="h-4 w-4">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"></path>
+                  </svg>
+                </p>
+              </th>
+
+              <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                  Status
+                </p>
+              </th>
+              <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="antialiased font-sans text-sm text-blue-gray-900 flex items-center justify-between gap-2 font-normal leading-none opacity-70">
+                  Actions
+                </p>
+              </th>
+            </tr>
+          </thead>
+          {!isLoading && (
+            <tbody>
+              {data?.map((item) => (
+                <SinglePrice item={item} key={item._id} />
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
