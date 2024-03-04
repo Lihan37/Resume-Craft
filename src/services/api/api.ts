0fadfcec533cc2190a6fd543de30b8faed5a8318
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-03-05 01:48:01 +0600

    Extend API slice with new endpoints for pricing management
    This commit introduces several enhancements to the API slice:
    - Added new endpoints for pricing management:
      - : Retrieves all pricing information.
      - : Creates a new pricing entry.
      - : Updates an existing pricing entry.
      - : Deletes a pricing entry.
    - Updated tagTypes to include 'allPayment' for caching.
    - Implemented cache invalidation strategies for new pricing endpoints.
    - Improved error handling and dispatching of user data upon successful payment creation.
    
    These changes expand the functionality of the API slice to facilitate the management of pricing data within the application. Additionally, appropriate cache invalidation strategies ensure data consistency across the application.

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setUser } from "../auth/authSlice";
+import { Pricing } from "../../types/paymentResponse";
 
 const apiSlice = createApi({
   reducerPath: "api",
@@ -8,7 +10,7 @@ const apiSlice = createApi({
     credentials: "include",
   }),
 
-  tagTypes: ["paymentHistory", "price"],
+  tagTypes: ["paymentHistory", "price", "allPayment"],
 
   endpoints: (builder) => ({
     getPrice: builder.query({
@@ -37,6 +39,47 @@ const apiSlice = createApi({
         }
       },
     }),
+    getAllPrice: builder.query<Pricing[], void>({
+      query: () => `/payment/v1`,
+      providesTags: ["allPayment"],
+    }),
+
+    createPricing: builder.mutation({
+      query: (data) => ({
+        url: "/payment/v1",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["allPayment"],
+    }),
+    editPricing: builder.mutation({
+      query: ({ data, id }) => ({
+        url: `/payment/v1/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["allPayment"],
+    }),
+    deletePricing: builder.mutation({
+      query: (id) => ({
+        url: `/payment/v1/${id}`,
+        method: "DELETE",
+      }),
+
+      // Update getAllPrice cache data
+      async onQueryStarted(id, { dispatch, queryFulfilled }) {
+        const updateTaskCache = dispatch(
+          apiSlice.util.updateQueryData("getAllPrice", undefined, (tasks) => {
+            return tasks.filter((item) => item._id !== id);
+          })
+        );
+        try {
+          await queryFulfilled;
+        } catch (error) {
+          updateTaskCache.undo();
+        }
+      },
+    }),
   }),
 });
 
@@ -44,6 +87,10 @@ export const {
   useGetPriceQuery,
   useCreatePaymentMutation,
   useGetPaymentHistoryQuery,
+  useGetAllPriceQuery,
+  useCreatePricingMutation,
+  useEditPricingMutation,
+  useDeletePricingMutation,
 } = apiSlice;
 
 export default apiSlice;
